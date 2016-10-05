f546d952bc42cd7d52c599a904c80f1a1bfefbd1
ouzhou <627290897@qq.com>
2016-10-05 16:38:26 +0800

    add error handlers

--- src/app.js
+++ src/app.js
@@ -1,20 +1,39 @@
-import express from 'express';
+import express from 'express'
 import bodyParser from 'body-parser'
 import compress from 'compression'
 import helmet from 'helmet'
+import logger from 'morgan'
 
-const app = express();
+const app = express()
 
 // use middlewares
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(compress())
 app.use(helmet())
+app.use(logger('dev'))
 
+// routes 
 app.get('/', (req, res) => {
-    res.send(`hello world!`);
-});
+  res.send(`hello world!`)
+})
 
-let server = app.listen(process.env.PORT || '3000', () => {
-    console.log('server running http://localhost:3000');
-});
+// error handlers
+app.use((req, res, next) => {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({
+    msg: err.message,
+    err: err
+  })
+})
+
+// server
+let server = app.listen(3000, () => {
+  console.log('server running http://localhost:3000')
+})

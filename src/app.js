5f140f264d02459b32f2cd8fffbf23a2b4e706aa
tom gobich <tom@tomgobich.com>
2017-04-02 12:48:42 -0400

    added user signup handling

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
   host     : 'mysql.thumbnailtemplates.com',
   user     : 'tomgob',
   password : 'kbd%$vqeT23',
-  database : 'thumbtem'
+  database : 'vfour'
 })
 
 // use middlewares
@@ -25,18 +25,56 @@ app.use(cors({
   origin: 'http://localhost:4200'
 }))
 
-// Helpers
+// SQL Helpers
 const allVTemplates = 'SELECT * FROM VTemplates WHERE intImageSortOrder = 1';
+
+// Authentication Routes
+app.post('/user/create', (req, res) => {
+  let user = req.body
+
+  let strUserID         = user.strUserID
+     ,strUsername       = user.strUsername
+     ,strEmail          = user.strEmail
+     ,blnEmailVerified  = strBlnToNum(user.blnEmailVerified)
+     ,strPassword       = user.strPassword    || ''
+     ,strAvatar         = user.strAVatar      || ''
+     ,strBio            = user.strBio         || ''
+     ,strYouTube        = user.strYouTube     || ''
+     ,strTwitter        = user.strTwitter     || ''
+     ,strFacebook       = user.strFacebook    || ''
+     ,intStatusID       = user.intStatusID    || 1
+
+  if ((!checkNullOrEmpty(strEmail)) && (!checkNullOrEmpty(strUserID))) {
+    let sql  = `INSERT INTO TUsers (strUserID, strUsername, strEmail, blnEmailVerified, strPassword, strAvatar, strBio, strYouTube, strTwitter, strFacebook, intStatusID) ` +
+               `VALUES ('${strUserID}', '${strUsername}', '${strEmail}', ${blnEmailVerified}, '${strPassword}', '${strAvatar}', '${strBio}', '${strYouTube}', '${strTwitter}', '${strFacebook}', ${intStatusID})`
+
+    connection.query(sql, (error, results, fields) => {
+      if (error) throw error
+      if (results) {
+        console.log('User Created: ', results);
+      }
+    })
+  }
+})
 
 // Routes
 app.get('/thumbnails', (req, res) => {
-  let sql = `${allVTemplates} ORDER BY dteDate DESC`;
+  let sql = `${allVTemplates} ORDER BY dteTemplateReleaseDate DESC, intTemplateSortOrder DESC`;
 
-  connection.query(sql, function (error, results, fields) {
+  connection.query(sql, (error, results, fields) => {
     if (error) throw error
     res.send(results)
-  });
+  })
 })
+
+// Utilities
+function checkNullOrEmpty(string) {
+  return string === null || string === undefined || string === ''
+}
+
+function strBlnToNum(bln) {
+  return bln ? 1: 0
+}
 
 // error handlers
 app.use((req, res, next) => {
@@ -55,5 +93,5 @@ app.use((err, req, res, next) => {
 
 // server
 let server = app.listen(3000, () => {
-  console.log('server running http://localhost:5000')
+  console.log('server running http://localhost:3000')
 })

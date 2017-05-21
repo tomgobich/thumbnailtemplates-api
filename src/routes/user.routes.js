e1fb10d8bb38962bb1e78d7b0cbfe5cc4cb8561f
Tom Gobich <tom@tomgobich.com>
2017-05-20 20:08:58 -0400

    Added params checks to prevent runtime errors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,12 +6,18 @@ import Utilities from '../utilities/utilities'
 // Gets a single user's details by strUserID
 exports.getUserDetailsByUserID = ((req, res) => {
   let uid = Utilities.escapeHtml(req.params.uid)
-  let sql = `SELECT * FROM TUsers WHERE strUserID = '${uid}'`
 
-  DB.connection.query(sql, (error, results, fields) => {
-    if (error) throw error
-    res.send({ user: results[0] })
-  })
+  if (!Utilities.checkNullOrEmpty(uid)) {
+    let sql = `SELECT * FROM TUsers WHERE strUserID = '${uid}'`
+
+    DB.connection.query(sql, (error, results, fields) => {
+      if (error) throw error
+      res.send({ user: results[0] })
+    })
+  }
+  else {
+    res.send({ error: 'Error: getUserDetailsByUserID, uid could not be found'})
+  }
 })
 
 exports.getUserProfileByUserID = ((req, res) => {
@@ -24,13 +30,18 @@ exports.getUserProfileByUserID = ((req, res) => {
 // Gets a single user's details by strUsername
 exports.getUserDetailsByUsername = ((req, res) => {
   let username = Utilities.escapeHtml(req.params.username)
-  let sql = `SELECT * FROM TUsers WHERE strUsername = '${username}'`
 
-  DB.connection.query(sql, (error, results, fields) => {
-    if (error) throw error
-    res.send({ user: results[0] })
-    return
-  })
+  if (!Utilities.checkNullOrEmpty(username)) {
+    let sql = `SELECT * FROM TUsers WHERE strUsername = '${username}'`
+
+    DB.connection.query(sql, (error, results, fields) => {
+      if (error) throw error
+      res.send({ user: results[0] })
+    })
+  }
+  else {
+    res.send({ error: 'Error: getUserDetailsByUsername, username could not be found'})
+  }
 })
 
 
@@ -38,12 +49,19 @@ exports.getUserDetailsByUsername = ((req, res) => {
 // Gets the username of a single user
 exports.getUserUsername = ((req, res) => {
   let uid = Utilities.escapeHtml(req.params.uid)
-  let sql = `SELECT strUsername FROM TUsers WHERE strUserID = '${uid}'`
 
-  DB.connection.query(sql, (error, results, fields) => {
-    if (error) throw error
-    res.send({ username: results[0].strUsername })
-  })
+  if (!Utilities.checkNullOrEmpty(uid)) {
+    let sql = `SELECT strUsername FROM TUsers WHERE strUserID = '${uid}'`
+
+    DB.connection.query(sql, (error, results, fields) => {
+      if (error) throw error
+      res.send({ username: results[0].strUsername })
+      console.log(results)
+    })
+  }
+  else {
+    res.send({ error: 'Error: getUserUsername, uid could not be found.'})
+  }
 })
 
 
@@ -51,24 +69,36 @@ exports.getUserUsername = ((req, res) => {
 // Returns whether provided username is unique
 exports.postIsUniqueUser = ((req, res) => {
   let username = Utilities.escapeHtml(req.body.username)
-  let sql = `SELECT strUsername FROM TUsers WHERE strUsername = '${username}'`
 
-  DB.connection.query(sql, (error, results, fields) => {
-    if (error) throw error
-    res.send({ unique: results.length === 0 ? true : false })
-  })
+  if (!Utilities.checkNullOrEmpty(username)) {
+    let sql = `SELECT strUsername FROM TUsers WHERE strUsername = '${username}'`
+
+    DB.connection.query(sql, (error, results, fields) => {
+      if (error) throw error
+      res.send({ unique: results.length === 0 ? true : false })
+    })
+  }
+  else {
+    res.send({ error: 'Error: postIsUniqueUser, username could not be found'})
+  }
 })
 
 
 
 exports.postIsUniqueEmail = ((req, res) => {
   let email = Utilities.escapeHtml(req.body.email)
-  let sql = `SELECT strEmail FROM TUsers WHERE strEmail = '${email}'`
 
-  DB.connection.query(sql, (error, results, fields) => {
-    if (error) throw error
-    res.send({ unique: results.length === 0 ? true : false })
-  })
+  if (!Utilities.checkNullOrEmpty(email)) {
+    let sql = `SELECT strEmail FROM TUsers WHERE strEmail = '${email}'`
+
+    DB.connection.query(sql, (error, results, fields) => {
+      if (error) throw error
+      res.send({ unique: results.length === 0 ? true : false })
+    })
+  }
+  else {
+    res.send({ error: 'Error: postIsUniqueEmail, email could not be found'})
+  }
 })
 
 
@@ -77,33 +107,38 @@ exports.postIsUniqueEmail = ((req, res) => {
 exports.postCreateUser = ((req, res) => {
   let user = req.body
 
-  let strUserID         = Utilities.escapeHtml(user.strUserID)
-  let strUsername       = Utilities.escapeHtml(user.strUsername)
-  let strEmail          = Utilities.escapeHtml(user.strEmail)
-  let blnEmailVerified  = Utilities.escapeHtml(user.blnEmailVerified) || 0
-  let strPassword       = Utilities.escapeHtml(user.strPassword)      || ''
-  let strAvatar         = Utilities.escapeHtml(user.strAVatar)        || ''
-  let strBio            = Utilities.escapeHtml(user.strBio)           || ''
-  let strYouTube        = Utilities.escapeHtml(user.strYouTube)       || ''
-  let strTwitter        = Utilities.escapeHtml(user.strTwitter)       || ''
-  let strFacebook       = Utilities.escapeHtml(user.strFacebook)      || ''
-  let intStatusID       = Utilities.escapeHtml(user.intStatusID)      || 1
+  if(!Utilities.checkNullOrEmpty(user)) {
+    let strUserID         = Utilities.escapeHtml(user.strUserID)
+    let strUsername       = Utilities.escapeHtml(user.strUsername)
+    let strEmail          = Utilities.escapeHtml(user.strEmail)
+    let blnEmailVerified  = Utilities.escapeHtml(user.blnEmailVerified) || 0
+    let strPassword       = Utilities.escapeHtml(user.strPassword)      || ''
+    let strAvatar         = Utilities.escapeHtml(user.strAVatar)        || ''
+    let strBio            = Utilities.escapeHtml(user.strBio)           || ''
+    let strYouTube        = Utilities.escapeHtml(user.strYouTube)       || ''
+    let strTwitter        = Utilities.escapeHtml(user.strTwitter)       || ''
+    let strFacebook       = Utilities.escapeHtml(user.strFacebook)      || ''
+    let intStatusID       = Utilities.escapeHtml(user.intStatusID)      || 1
 
-  if ((!Utilities.checkNullOrEmpty(strEmail)) && (!Utilities.checkNullOrEmpty(strUserID))) {
-    let sql  = `INSERT INTO TUsers (strUserID, strUsername, strEmail, blnEmailVerified, strPassword, strAvatar, strBio, strYouTube, strTwitter, strFacebook, intStatusID) ` +
-               `VALUES ('${strUserID}', '${strUsername}', '${strEmail}', ${blnEmailVerified}, '${strPassword}', '${strAvatar}', '${strBio}', '${strYouTube}', '${strTwitter}', '${strFacebook}', ${intStatusID})`
+    if ((!Utilities.checkNullOrEmpty(strEmail)) && (!Utilities.checkNullOrEmpty(strUserID))) {
+      let sql  = `INSERT INTO TUsers (strUserID, strUsername, strEmail, blnEmailVerified, strPassword, strAvatar, strBio, strYouTube, strTwitter, strFacebook, intStatusID) ` +
+                `VALUES ('${strUserID}', '${strUsername}', '${strEmail}', ${blnEmailVerified}, '${strPassword}', '${strAvatar}', '${strBio}', '${strYouTube}', '${strTwitter}', '${strFacebook}', ${intStatusID})`
 
-    DB.connection.query(sql, (error, results, fields) => {
-      if (error) throw error
+      DB.connection.query(sql, (error, results, fields) => {
+        if (error) throw error
 
-      res.send(JSON.stringify({
-        data: results,
-        message: `Welcome to ThumbnailTemplates ${strUsername}!`
-      }))
-    })
+        res.send(JSON.stringify({
+          data: results,
+          message: `Welcome to ThumbnailTemplates ${strUsername}!`
+        }))
+      })
+    }
+    else {
+      let error = "Empty or harmful data was found. Please complete all required fields, remove special characters, and try again"
+      res.send(JSON.stringify({ error }))
+    }
   }
   else {
-    let error = "Some fields were invalid, please correct the validation messages and try again!"
-    res.send(JSON.stringify({ error }))
+    res.send({ error: 'Error: postCreateUser, user data could not be found'})
   }
 })
